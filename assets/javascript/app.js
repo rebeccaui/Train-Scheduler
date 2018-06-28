54e0b8f65bdd501a9927b59c07be13f9472c184e
rwieberdink <rebeccawieberdink@gmail.com>
2018-06-28 15:41:43 -0400

    test functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,24 +11,24 @@ var config = {
   };
   firebase.initializeApp(config);
 
-//Variables 
+//Variables - Database 
 var database = firebase.database();
 var trainDB = database.ref("/trains");
-var trainName = "";
+//Variables - User Input
+var trainName = ""; 
 var trainDest = "";
 var trainTime = "";
 var trainFreq = "";
+//Variable to hold all children added to the database for a single train
 var trainFacts = "";
-
+//Variables - moment() and manipulated 
 var currentTime = moment();
-var nextArrival = "";  //arrival time of nextTrain - trainFreq - difference%trainFreq loop?
-var trainMins = "";  //Displayed/Decremented Number of minutes til next train. 
-            //Starts from difference 
-var difference = ""; //Equivalent to trainFreq, 
-var trainTimeConverted = "";
-var remainder = "";
-
-//var trainMins = "..." //moment(). trainTime + trainFreq
+var nextArrival = "";  //Arrival time of the next train, displays in table (column 4), determined by trainFreq and currentTime
+var nextArrivalConverted = "";
+var trainMins = "";  //Decrementing number of minutes until the next train, displayed in table (column 5), determined by trainFreq, initial difference, and remainder  
+var difference = ""; //The difference between the currentTime and trainTime, not displayed 
+var trainTimeConverted = ""; //
+var remainder = ""; //The remainder from which we find the remaining trainMins until nextArrival
 
     //Add Train event
     $("#addTrain").click(function(event){
@@ -36,93 +36,89 @@ var remainder = "";
         //Set variables to the value of what the user types in
         trainName = $("#userTrain").val().trim();
         trainDest = $("#userDest").val().trim();
-        trainTime = $("#userTime").val().trim();
-        trainFreq = parseInt($("#userFreq").val().trim());    
-        
+        trainTime = moment($("#userTime").val().trim(), "HH:mm").format("X");
+        trainFreq = $("#userFreq").val().trim();    
+
+    if(trainName != "" && trainDest != "" && trainTime != "" && trainFreq != "") {
         console.log("Train Name: " + trainName);
         console.log("Train Destination: " + trainDest);
-        console.log("Starting Train Time: " + trainTime);
         console.log("Train Frequency: " + trainFreq + " mins");
-       
-       
-     /*    //for (i = 0; i = trainFreq.val(); i--){
-            if (difference === 0){
-
-                $("#minsAway").text(trainFreq); //maybe wrong
-                //Find the time of the next train
-                nextTrain = currentTime.subtract(difference);
-            }
-        }
-            
-        var currentTime = moment();
-    
-        //arrival time of nextTrain - trainFreq - difference%trainFreq loop?
-        var trainMins = "";  //Displayed/Decremented Number of minutes til next train. 
-                //Starts from difference 
-        var difference = ""; //Equivalent to trainFreq[i]
-        //var trainMins = "..." //moment(). trainTime + trainFreq    */
-
-        //Push user data to database
-        trainDB.push({
-            trainName,
-            trainDest,
-            trainTime,
-            trainFreq
-            //trainMins
-        });
+        console.log("Starting Train Time: " + trainTime);
+        console.log("Next Arrival: " + nextArrival);
 
         //Change the input values back to an empty string
         $("#userTrain").val("");
         $("#userDest").val("");
         $("#userTime").val("");
         $("#userFreq").val("");
+        $("#trainMins").val("");
 
-    
+        //Push user input variables and moment() to database
+        currentTime = moment().format("X");
+        trainDB.push({
+            trainName,
+            trainDest,
+            trainTime,
+            trainFreq,
+            currentTime
+        });
+    } else {
+        return false;
+      }
     });
 
     trainDB.on("child_added", function(childSnapshot){
         trainFacts = childSnapshot.val();
-            trainName = trainFacts.trainName;
-            trainDest = trainFacts.trainDest;
+            trainName = trainFacts.trainName;   //required for display
+            trainDest = trainFacts.trainDest;   //required for display
             trainTime = trainFacts.trainTime;
-            trainFreq = trainFacts.trainFreq;
+            trainTimeConverted = moment.unix(trainFacts.trainTime);
+            //The full difference of trainTime and currentTime
+            difference = moment().diff(moment(trainTimeConverted, "HH:mm"), "minutes");
+            trainFreq = trainFacts.trainFreq;   //required for display
+            //The difference is divided by the trainFreq to determine when each nextArrival occurs, then finding the remainder
+            remainder = difference % parseInt(trainFacts.trainFreq);
+            //Minutes Away until nextArrival will be trainFreq minus the remainder
+            trainMins = parseInt(trainFacts.trainFreq) - remainder;  //required for display
+            
+            //nextArrival = trainFacts.nextArrival;
+            //nextArrivalConverted = nextArrivalConverted;   //required for display
 
-            //The difference of trainTime and currentTime
-       difference = moment().diff(moment(trainTime), "minutes");
-       
-       trainTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
-       //The remainder is the difference divided by the trainFreq
-       remainder = difference % trainFreq;
-       //Minutes Away will be the trainFreq minus the remainder
-       trainMins = trainFreq - remainder;
 
+            //Convert the trainTime to a certain format and subtract a year
+            //trainTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+            
+            //Convert the nextArrival time to the standard format
+            //nextArrivalConverted = moment(nextArrival).format("HH:mm");
+            
+            if(difference > 0) {
+            //The nextArrival will be equal to the currentTime plus the trainMins
+            nextArrival = moment().add(trainMins, "minutes").format("hh:mm A");
+            } else {
+                nextArrival = trainTimeConverted.format("hh:mm A");
+                trainMins = Math.abs(difference - 1);
+            }
+            
             var newTrain = $("<tr>").addClass("train");
             newTrain.append(
-                $("<td>").text(trainName),
-                $("<td>").text(trainDest),
-                $("<td>").addClass("freq").text(trainFreq),
-                $("<td>").addClass("nextArrival").text(trainMins)
+                $("<td>").text(trainFacts.trainName),
+                $("<td>").text(trainFacts.trainDest),
+                $("<td>").addClass("freq").text(trainFacts.trainFreq),
+                $("<td>").addClass("nextArrival").text(nextArrival),
+                $("<td>").addClass("minsAway").text(trainMins)
             );
             $("#trainTable").append(newTrain);
-            console.log("Difference: " + difference);
-            console.log(trainTimeConverted);
-            console.log("Minutes Away (decrementing): " + trainMins);
-
-
     });
-
 });
 
 
-//(".table").attr("sortable");
+/* (".table").attr("sortable");
 
 //Insert search bar for table
 /* var allRows = $("tr");
 $("input#search").on("keydown keyup", function() {
   allRows.hide();
   $("tr:contains('" + $(this).val() + "')").show();
-}); */
+}); 
 
-// .checkvalidity() returns T/F  .reportvalidity() tells user
-
-//Set trainTime 
+// .checkvalidity() returns T/F  .reportvalidity() tells user */
